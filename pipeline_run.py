048c5b5f9878aa34721c0979ccfc138c83efffe3
bastinjob <bastinjob1998@gmail.com>
2024-06-29 02:11:22 -0400

    just some fancy print statements

--- pipeline_run.py
+++ pipeline_run.py
@@ -1,7 +1,12 @@
 from preprocess import preprocess_data
 from train import train_random_forest_classifier
 
+print('Reading and Preprocessing dataset')
+
 df = preprocess_data(dataset_path='data/bmi_train.csv')
+
+print('Commencing training of random forest classifier')
+
 train_random_forest_classifier(df)
 
 print('Run Successful!')
