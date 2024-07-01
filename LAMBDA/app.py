44a2d58e896c7b0c4046c12d8577f8cb13d45952
bastinjob <bastinjob1998@gmail.com>
2024-07-01 10:36:48 -0400

    added configs for LAmbda serveleress deployemnt, but need size reduction

---
+++ LAMBDA/app.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import pickle
+#from preprocess import preprocess_data
+
+from flask import Flask,request, jsonify, render_template
+
+
+#initialize the app
+
+app = Flask(__name__)
+
+
+with open('models/random_forest_classifier_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('preprocess_models/scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+dict_bmi = {
+            0: 'Extremely Weak',
+            1: 'Weak',
+            2: 'Normal',
+            3: 'Overweight',
+            4: 'Obese',
+            5: 'Extremely Obese'
+        }
+
+def preprocess_inputs(gender, height, weight):
+
+    
+    gender_encoded  = 1. if gender.lower() == 'male' else 0.
+
+    input_df = pd.DataFrame({'Gender_encoded':[gender_encoded], 'Height':[height], 'Weight':[weight]})
+
+    scaled_features = scaler.fit_transform(input_df[['Height', 'Weight']])
+
+    # Create a DataFrame with the scaled features
+    df_scaled = pd.DataFrame(scaled_features, columns=['Height_scaled', 'Weight_scaled'])
+
+    # Concatenate with the original DataFrame (if needed)
+    df_final = pd.concat([input_df.drop(columns=['Height','Weight'],axis=1), df_scaled], axis=1)
+
+    return df_final.values
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if 'file' in request.files:
+        file = request.files['file']
+        df = pd.read_csv(file)
+        df['Gender_encoded'] = df['Gender'].apply(lambda gender: 1 if gender.lower() == 'male' else 0)
+        scaled_features = scaler.transform(df[['Height', 'Weight']])
+        df[['Height', 'Weight']] = scaled_features
+        predictions = model.predict(df[['Gender_encoded', 'Height', 'Weight']])
+        df['BMI Category'] = predictions
+        df['Obesity Level'] = df['BMI Category'].apply(lambda pred: dict_bmi[pred])
+
+        #return jsonify({'BMI Category':predictions.tolist(),'Obesity Level': Obesity_levels })
+
+        result_html = df.to_html(classes='table table-striped')
+
+        return render_template('result.html', table=result_html)
+
+    else:
+        data = request.form
+        gender = data.get('gender')
+        height = data.get('height')
+        weight = data.get('weight')
+
+        if gender is None or height is None or weight is None:
+            return jsonify({'error': 'Missing input data'}), 400
+
+        input_data = preprocess_inputs(gender, height, weight)
+        prediction = model.predict(input_data)
+
+        df = pd.DataFrame({'Gender':[gender], 'Height':[height], 'Weight':[weight]})
+        df['BMI Category'] = [prediction[0]]
+        df['Obesity Level'] = [dict_bmi[prediction[0]]]
+        
+        #obesity = dict_bmi[prediction]
+        #return jsonify({'BMI': prediction})
+
+        result_html = df.to_html(classes='table table-striped')
+
+        return render_template('result.html', table=result_html)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
